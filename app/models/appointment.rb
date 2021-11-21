dfcea4956f7f75d5d482950347d9e96620fcd7e9
Matias Ocares <matiasn.ocares@gmail.com>
2021-11-21 16:02:41 -0300

    Validar fecha pasada y turnos correctos

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,4 +5,22 @@ class Appointment < ApplicationRecord
   validates :name, presence: true
   validates :surname, presence: true
   validates :phone, presence: true
+
+  validate :past_date
+  validate :right_time,
+
+  def past_date
+    if(date < DateTime.now)
+      errors.add(:date, "La fecha ingresada es pasada.")
+    end
+  end
+
+  def right_time #valida que la hora del turno sea correcto (hora en punto de 8 a 17 hs.)
+    time = date.strftime("%H:%M")
+    turnos =['08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00',]
+    if(!turnos.include?(time))
+      errors.add(:date, "La hora elegida es incorrecta. Debe ser hora exacta entre 08:00 a 17:00.")
+    end
+  end
+
 end
