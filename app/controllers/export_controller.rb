40a01e5ad209a474d8d1b97fb7d02b085bf5872e
Matias Ocares <matiasn.ocares@gmail.com>
2021-11-27 18:58:20 -0300

    Exportar grilla semanal

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -3,14 +3,45 @@ class ExportController < ApplicationController
 
     def index
         if params[:date].present?
-            if params[:name].present?
-                ex = ExportHelper::Export.new(params[:date])
-                ex.exportprof(params[:date], params[:name])
+            if !params[:week] #Sólo exporta un día.
+                if params[:name].present?
+                    ex = ExportHelper::Export.new(params[:date])
+                    ex.exportprof(params[:date], params[:name])
+                else
+                    ex = ExportHelper::Export.new(params[:date])
+                    ex.export(params[:date])
+                end
             else
-                ex = ExportHelper::Export.new(params[:date])
-                ex.export(params[:date])
+                #Exporta semana completa
+                date = get_monday(params[:date])
+                week = get_array_week(date)
+                if params[:name].present?
+                    ex = ExportHelper::Export.new(week)
+                    ex.exportweekprof(week, params[:name])
+                else
+                    ex = ExportHelper::Export.new(week)
+                    ex.exportweek(week)
+                end
+                
             end
+        else
+            #Acá iría error que falta ingresar fecha.
         end
     end
+
+    def get_monday(date)
+        date = Date.parse date
+        date = date -((date.wday - 1)%7)
+        return date
+      end
+
+    def get_array_week(date)
+        week=[]
+        for a in 1..5 do          
+        day = date.next_day(a)
+        week << day.to_s
+        end
+        return week    
+      end
 
 end
--- app/helpers/export_helper.rb
+++ app/helpers/export_helper.rb
@@ -1,6 +1,5 @@
 require 'erb'
 require 'export_controller'
-
 module ExportHelper
 
   def display_professionals
@@ -212,5 +211,173 @@ end
     return "Se generó el reporte solicitado con nombre: "+reporte+" en el directorio #{Dir.home()+"/"}"
    
     end
+
+
+    #---------------WEEK-----------------------------
+    def get_template_week()
+      # Create template.
+        return template = %{
+        <html>
+       
+          <html>
+      <head>
+        <style>
+          table {
+            border-collapse: collapse;
+          }
+          td, th {
+            border: 1px solid black;
+            padding: 1em;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Grilla de Turnos</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th id="08">08:00</th>
+              <th id="09">09:00</th>
+              <th id="10">10:00</th>
+              <th id="11">11:00</th>
+              <th id="12">12:00</th>
+              <th id="13">13:00</th>
+              <th id="14">14:00</th>
+              <th id="15">15:00</th>
+              <th id="16">16:00</th>
+              <th id="17">17:00</th>
+            </tr>
+          </thead>
+          <tbody>
+          <% @date.each do |day|%>
+          <tr>
+          <td id="date"> <%= day %> </td>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "08:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 08" > <%= @name[turno] %></td>
+          <% else%>
+          <td headers="date 08" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "09:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 09" > <%= @name[turno]%></td>
+          <% else%>
+          <td headers="date 09" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "10:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 10" > <%= @name[turno] %></td>
+          <% else%>
+          <td headers="date 10" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "11:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 11" > <%= @name[turno]%></td>
+          <% else%>
+          <td headers="date 11" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "12:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 12" > <%= @name[turno] %></td>
+          <% else%>
+          <td headers="date 12" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "13:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 13" > <%=  @name[turno]%></td>
+          <% else%>
+          <td headers="date 13" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "14:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 14" > <%= @name[turno] %></td>
+          <% else%>
+          <td headers="date 14" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "15:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 15" > <%= @name[turno] %></td>
+          <% else%>
+          <td headers="date 15" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "16:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 16" > <%= @name[turno]%></td>
+          <% else%>
+          <td headers="date 16" > <%= %></td>
+          <%end%>
+          <% turno= @fulldate.find_index{|t| t.split(" ")[1] == "17:00" && t.split(" ")[0]== day} %>
+          <% if (turno!=nil)%>
+          <td headers="date 17" > <%= @name[turno] %></td>
+          <% else%>
+          <td headers="date 17" > <%= %></td>
+          <%end%>
+          
+    
+          </tr>
+          <%end%>
+          </tbody>
+        </table>
+      </body>
+    </html>
+    
+      </html>
+    }.gsub(/^  /, '')
+    end
+
+
+    def exportweekprof(week, professional)
+
+
+      rhtml = ERB.new(get_template_week)
+    
+      # Set up template data.
+                       
+    
+      lista =[]                
+      week.each do |date|
+        fecha=Date.parse(date)               
+        lista << Appointment.includes(:professional).where(:date=> fecha.beginning_of_day..fecha.end_of_day, professional: {name: professional})
+      end
+      lista.each do |clave|
+        self.add_feature(clave[:date],clave[:name], clave[:surname], clave[:phone], clave.professional.name)
+      end
+    
+    # Produce result.
+      rhtml.run(self.get_binding)
+      reporte ="reporte"+rand(999).to_s+".html"
+      File.open(Dir.home()+"/"+reporte, "w") do |fichero|
+        fichero.write(rhtml.result(self.get_binding))
+      end
+      return "Se generó el reporte solicitado con nombre: "+reporte+" en el directorio #{Dir.home()+"/"}"    
+      end
+    
+      def exportweek(week)
+    
+    
+        rhtml = ERB.new(get_template_week)
+      
+        # Set up template data.
+         lista =[]                
+         week.each do |date|
+          fecha=Date.parse(date)               
+          lista << Appointment.includes(:professional).where(:date=> fecha.beginning_of_day..fecha.end_of_day)
+        end
+        #INTENTO JUNTAR MISMAS FECHAS puts lista.sort{|k,p| k["phone"]==p["phone"]}  
+        lista.each do |other|
+          other.each do |clave|
+          self.add_feature_all(clave[:date],clave[:name], clave[:surname], clave[:phone], clave.professional.name)
+        end
+      end
+      
+      # Produce result.
+        rhtml.run(self.get_binding)
+        reporte ="reporte"+rand(999).to_s+".html"
+        File.open(Dir.home()+"/"+reporte, "w") do |fichero|
+          fichero.write(rhtml.result(self.get_binding))
+        end
+        return "Se generó el reporte solicitado con nombre: "+reporte+" en el directorio #{Dir.home()+"/"}"       
+        end
 end
 end
