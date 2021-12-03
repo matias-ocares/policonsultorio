c29a2acdda758d932e2c52ebecd4a875c572f115
Matias Ocares <matiasn.ocares@gmail.com>
2021-12-03 18:12:57 -0300

    Config roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_214252) do
+ActiveRecord::Schema.define(version: 2021_12_03_203802) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'admin@policon.com', password: '123456')
+User.create(email: 'admin@policon.com', password: '123456', role: 0)
