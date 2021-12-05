fa5c1a775d22e0d7030a854fa5307897a25579b3
Matias Ocares <matiasn.ocares@gmail.com>
2021-12-05 16:44:06 -0300

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'consul@policon.com', password: '123456', role: 0)
-User.create(email: 'asist@policon.com', password: '123456', role: 1)
-User.create(email: 'admin@policon.com', password: '123456', role: 2)
+User.create(email: 'consul@policon.com', password: '123456', role: 0) #Rol Consulta
+User.create(email: 'asist@policon.com', password: '123456', role: 1) #Rol Asistencia
+User.create(email: 'admin@policon.com', password: '123456', role: 2) #Rol Administración
 
 prof1 = Professional.create(name: "Juan Fernandez")
 prof2 = Professional.create(name: "Maria Perez")
