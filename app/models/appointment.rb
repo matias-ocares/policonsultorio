b6b14b1a02104af6275f2e51903abcbbd6fdb694
Matias Ocares <matiasn.ocares@gmail.com>
2021-12-05 14:13:24 -0300

    Mejoras validaciones formulario

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,7 +4,7 @@ class Appointment < ApplicationRecord
   validates :date, presence: true, uniqueness: {scope: :professional, message: "El turno ya se encuentra tomado para ese profesional."}, length: {minimum: 2}
   validates :name, presence: true
   validates :surname, presence: true
-  validates :phone, presence: true
+  validates :phone, presence: true, numericality: true, length: {minimum: 8, maximum: 15}
 
   validate :past_date
   validate :right_time,
